de1afe98d2f5ca21a4812fe307fd151ca7cdec41
Nikita Dyomin <niko.demin@gmail.com>
2018-11-02 19:19:26 +0300

    some shit happens

--- main.cpp
+++ main.cpp
@@ -2,14 +2,41 @@
 #include <ctime>
 #include <LIMITS.H>
 #include	<math.h>
+#include <sys/time.h>
+#include <queue>
 using namespace std;
 
-const int range = 15;
-const int arrSize = 150;
+const int range = 10;
+const int arrSize = 100;
 int avrg = 0;
+
+class profilingClass
+{
+	queue<double> periods;
+	double timeBegin;
+	struct timeval tv;
+public:
+	void startCount()
+	{
+		gettimeofday(&tv, NULL);
+		timeBegin = ((double)tv.tv_sec) *1000 +((double)tv.tv_usec)/1000;
+	}
+	void stopCount()
+	{
+		gettimeofday(&tv, NULL);
+		periods.push( ((double)tv.tv_sec) *1000 + ((double)tv.tv_usec)/1000 - timeBegin );
+	}
+	double getElem()
+	{
+		double r = periods.front();
+		periods.pop();
+		return r;
+	}
+}pc;
 
 int get_dev1(int* arr)		//in forehead solution
 {	
+	//pc.startCount();
 	int dev = 0;
 	for (int i = 0; i < arrSize; ++i)
 	{
@@ -17,6 +44,7 @@ int get_dev1(int* arr)		//in forehead solution
 	}
 	dev /= arrSize-1;
 	dev = sqrt(dev);
+	//pc.stopCount();
 	return dev;
 }
 
@@ -24,6 +52,7 @@ int sqrSumm = 0;
 int summ = 0;
 int get_dev2(int* arr)
 {
+	pc.startCount();
 	int dev = 0;
 	for (int i = 0; i < arrSize; ++i)
 	{
@@ -34,23 +63,29 @@ int get_dev2(int* arr)
 	
 	dev = (sqrSumm - summ)/arrSize-1;
 	dev = sqrt(dev);
+	pc.stopCount();
 	return dev;
 }
 
 int histogramm[range] = {0};
 int get_dev3(int* arr)
 {
+	pc.startCount();
 	int dev = 0;
+	cout<< "gg"<<endl;
 	for (int i = 0; i < arrSize; ++i)		// fill histogramm
 	{
+		cout << arr[i]-1<<' ';
 		histogramm[arr[i]-1] += 1;
 	}
+	cout << endl<< "gg2";
 	for (int i = 0; i < range; ++i)
 	{
 		dev += pow(i-avrg, 2)*histogramm[i];
 	}
 	dev /= arrSize-1;
 	dev = sqrt(dev);
+	pc.stopCount();
 	return dev;
 }
 
@@ -88,9 +123,9 @@ int main()
 	avrg /= arrSize;	//prepairing complete
 	
 	cout <<endl<<"average = " << avrg << endl;
-	cout << "first deviation = "<<get_dev1(Arr) << endl;
-	cout << "second deviation = "<<get_dev2(Arr) << endl;
-	cout << "third deviation = "<<get_dev3(Arr) << endl;
+	/*cout << "first deviation = "<<get_dev1(Arr) << " time = "<<endl;
+	cout << "second deviation = "<<get_dev2(Arr) << " time = "<< pc.getElem() <<endl;*/
+	cout << "third deviation = "<<get_dev3(Arr) << " time = "<< pc.getElem() <<endl;
 	cout << "histogramm:" << endl;
 	printHistogramm();
 	countAverage();
